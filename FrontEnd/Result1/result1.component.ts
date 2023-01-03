923f5d644746df9ecfa3b37e23cf3bb7afc19750
rushi20139 <87067394+rushi20139@users.noreply.github.com>
2023-01-03 21:18:37 +0530

    Update result1.component.ts

--- FrontEnd/Result1/result1.component.ts
+++ FrontEnd/Result1/result1.component.ts
@@ -1,7 +1,7 @@
 
 import { Component, Onlnit } from '@angular/core';
 import { SharedServiceService } from 'src/shared/shared-service.service';
-<!--- This is the code for Displaying Result Table --->
+/*This is the code for Displaying Result Table */
 <!-- This is the code for Displaying Result Table -->
 export interface PeriodicElement {
 name: string;
